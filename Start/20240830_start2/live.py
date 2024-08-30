d2d04a2fc3d5f08fcf4629bb2026de979e714018
Riis-KIM <gunil099@gmail.com>
2024-08-30 17:33:15 +0900

    t

--- Start/20240830_start2/live.py
+++ Start/20240830_start2/live.py
@@ -15,3 +15,14 @@ print(f'부분집합의 수 : {1<<len(arr)}')
 for i in range(1<<len(arr)):
     print(i, end= ' ')
 print()
+
+# i & (1 << N) : N번째 비트가 0인지 아닌지 알 수 있다.
+
+# i 의미 : i 번째 부분집합
+for i in range(1<<len(arr)):
+    for idx in range(len(arr)):
+        # i & (1<<idx)
+        # - i 번째 부분집합에 idx 요소가 포함되어 있나ㅣ요?
+        if i & (1<<idx):
+            print(arr[idx], end=' ')
+    print()
